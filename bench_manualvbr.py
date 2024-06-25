340195d307fa08da37c2f620ab108d09d567a42f
Pratyush Das <reikdas@gmail.com>
2024-06-25 22:21:41 +0000

    Benchmark real world matrices

---
+++ bench_manualvbr.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+import pathlib
+
+FILEPATH = pathlib.Path(__file__).resolve().parent
+BASE_PATH = os.path.join(FILEPATH, "..")
+
+if __name__ == "__main__":
+    for threads in [1, 2, 4, 8, 16]:
+        with open(os.path.join("results", "bench_executor_manual_"+str(threads)+"thrds.csv"), "w") as f:
+            VBR_PATH = f"{BASE_PATH}/manual_mtx"
+            for filename in os.listdir(VBR_PATH):
+                assert(filename.endswith(".mtx"))
+                f.write(f"{filename[:-4]}")
+                print(f"Running {filename[:-4]} with {threads} threads")
+                output = subprocess.check_output(["python3", "run_matrix.py", "-m", os.path.join(VBR_PATH, filename), "-t", str(threads), "-b", "512", "-o", "temp.csv"]).decode("utf-8").split("\n")
+                start = -7
+                for i in range(5):
+                    print(f"{i}: {output[start+i]}")
+                    f.write(","+output[start+i])
+                f.write("\n")
